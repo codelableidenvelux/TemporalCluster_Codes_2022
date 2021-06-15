0a0fa87d653ab8f06e3b72ae0990c1a9840b653e
Enea <enea.ceolini@quantactions.com>
2021-06-15 16:45:59 +0200

    WIP connectionist again

--- connetionist_plot.m
+++ connetionist_plot.m
@@ -1,17 +1,3 @@
-presence = zeros(2500);
-color = zeros(2500);
-for i = 1:2500
-    for j = i:2500
-        if ~isempty(all_R_res{i, j})
-            presence(i, j) = all_R_res{i, j}.p < 0.05;
-            presence(j, i) = all_R_res{i, j}.p < 0.05;
-            color(i, j) = all_R_res{i, j}.invert;
-            color(j, i) = all_R_res{i, j}.invert;
-        end
-    end
-end
-
-
 %%
 % presence = rand(2500) > 0.9;
 % color = rand(2500) > 0.9;
@@ -19,22 +5,27 @@ end
 figure(1)
 hold on
 
+min_dist_pix = 5;
+th_dist = min_dist_pix * sqrt(2);
+th_R = 0.8;
+th_F = 5;
+
 for i = 1:2500
-    for j = i:2500
-        if mask(i, j) && abs(R_vals(i, j)) > 0.5
-        
-            x1 = mod(i - 1, 50);
-            y1 = int32((i - 1) / 50);
-            x2 = mod((j - 1), 50);
-            y2 = int32((j - 1) / 50);
+    for j = i + 1:2500
+        if mask(i, j) && abs(R_vals(i, j)) > th_R && F_vals(i, j) > th_F
             
-%             if mod(x1, 2) == 0 && mod(y1, 2) == 0 && mod(x2, 2) == 0 && mod(y2, 2) == 0
-                if betas(i, j, 1) > 0
-                    plot([x1, x2], [y1, y2], 'Color', [0,0,1, 0.1])
+            x1 = double(mod(i - 1, 50));
+            y1 = double(int32((i - 1) / 50));
+            x2 = double(mod((j - 1), 50));
+            y2 = double(int32((j - 1) / 50));
+            
+            if norm([x1; y1] - [x2; y2]) > th_dist
+                if sign(all_age_gender{1, 1}.mdl{i, 1}.betas(1)) ~= sign(all_age_gender{1, 1}.mdl{j, 1}.betas(1))
+                    plot([x1, x2], [y1, y2], 'Color', [0,0,1, 0.01])
                 else
-                    plot([x1, x2], [y1, y2], 'Color', [1,0,0, 0.1])
+                    plot([x1, x2], [y1, y2], 'Color', [1,0,0, 0.01])
                 end
-%             end
+            end
         end
         
     end
@@ -49,26 +40,26 @@ hold on
 counter_red = zeros(50);
 counter_blue = zeros(50);
 
-% 
+%
 for i = 0:2499
     for j = i:2499
         if mask(i, j)
-        
+            
             x1 = mod(i, 50) + 1;
             y1 = floor(i / 50) + 1;
             x2 = mod(j, 50) + 1;
             y2 = floor(j / 50) + 1;
             
             
-%             if mod(x1, 2) == 0 && mod(y1, 2) == 0 && mod(x2, 2) == 0 && mod(y2, 2) == 0
-                if betas(i + 1, j + 1, 1) > 0
-                    counter_red(x1, y1) = counter_red(x1, y1) + 1;
-                    counter_red(x2, y2) = counter_red(x2, y2) + 1;
-                else
-                    counter_blue(x1, y1) = counter_blue(x1, y1) + 1;
-                    counter_blue(x2, y2) = counter_blue(x2, y2) + 1;
-                end
-%             end
+            %             if mod(x1, 2) == 0 && mod(y1, 2) == 0 && mod(x2, 2) == 0 && mod(y2, 2) == 0
+            if betas(i + 1, j + 1, 1) > 0
+                counter_red(x1, y1) = counter_red(x1, y1) + 1;
+                counter_red(x2, y2) = counter_red(x2, y2) + 1;
+            else
+                counter_blue(x1, y1) = counter_blue(x1, y1) + 1;
+                counter_blue(x2, y2) = counter_blue(x2, y2) + 1;
+            end
+            %             end
         end
         
     end
